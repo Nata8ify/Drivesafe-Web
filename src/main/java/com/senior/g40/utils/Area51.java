c98409e24f4f8256bd31ab21f444db765eb3a6dc
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-08-29 12:27:35 +0700

    MAKE Duplicated Incident's report Predication Module
    Any Incidents which have reported will be filtering for duplicated (By
    report location range between each other < 30 meters)
    
    ** No compare on incident type (by now)

--- src/main/java/com/senior/g40/utils/Area51.java
+++ src/main/java/com/senior/g40/utils/Area51.java
@@ -3,6 +3,8 @@ package com.senior.g40.utils;
 import com.google.gson.Gson;
 import com.senior.g40.model.Accident;
 import com.senior.g40.model.extras.Feed;
+import com.senior.g40.model.extras.LatLng;
+import com.senior.g40.service.AccidentService;
 import com.senior.g40.service.FeedService;
 import com.senior.g40.service.StatisticService;
 import java.sql.Date;
@@ -27,16 +29,17 @@ public class Area51 {
         //   System.out.println(new Gson().toJson(StatisticService.getInstance().getTotalAccidentLatLng(1L)));
 //        int[] times = StatisticService.getInstance().getReportFreqSeries(null);
 //        System.out.println(gson.toJson(times));
-        FeedService fs = FeedService.getInstance();
-        fs.save(14, 564, Accident.ACC_CODE_A);
-        fs.save(1, 564, Accident.ACC_CODE_G);
-        fs.save(14, 565, Accident.ACC_CODE_R);
-        fs.save(14, 563, Accident.ACC_CODE_A);
-        fs.save(1, 563, Accident.ACC_CODE_G);
-        fs.save(14, 562, Accident.ACC_CODE_R);
+//        FeedService fs = FeedService.getInstance();
+//        fs.save(14, 564, Accident.ACC_CODE_A);
+//        fs.save(1, 564, Accident.ACC_CODE_G);
+//        fs.save(14, 565, Accident.ACC_CODE_R);
+//        fs.save(14, 563, Accident.ACC_CODE_A);
+//        fs.save(1, 563, Accident.ACC_CODE_G);
+//        fs.save(14, 562, Accident.ACC_CODE_R);
 //        for(Feed feed : fs.getFeeds(new Date(System.currentTimeMillis()), 5)){
 //            System.out.println(feed.toString());
 //        }
-        System.out.println(FeedService.getInstance().getFeeds(new Date(System.currentTimeMillis()), 5));
+//        System.out.println(FeedService.getInstance().getFeeds(new Date(System.currentTimeMillis()), 5));
+        System.out.println(AccidentService.getInstance().isNotNearByDuplicate(new LatLng(14.072801, 99.925347)));
     }
 }
